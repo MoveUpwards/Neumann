350eb6ad99d2c90b87d98965bf812333c49a8201
Damien <damien@slide-r.com>
2021-06-08 10:30:30 +0200

    Remove commented code and add documentation

--- Neumann/Sources/Pythagore/Pythagore.swift
+++ Neumann/Sources/Pythagore/Pythagore.swift
@@ -26,23 +26,23 @@ extension Pythagore {
         sqrt(pow(hypotenuse, 2) - pow(otherSide, 2))
     }
 
+    /// Adjacent side from the opposite side and the alpha angle
     public static func adjacent(opposite: Double, alpha: Double) -> Double {
         opposite / tan(alpha)
     }
 
+    /// Adjacent side from the hypotenuse side and the alpha angle
     public static func adjacent(hypotenuse: Double, alpha: Double) -> Double {
-//        alpha == acos(adjacent  / hypotenuse)
-//        cos(alpha) == adjacent  / hypotenuse
         return hypotenuse * cos(alpha)
     }
 
+    /// Opposite side from the adjacent side and the alpha angle
     public static func opposite(adjacent: Double, alpha: Double) -> Double {
         adjacent * tan(alpha)
     }
 
+    /// Opposite side from the hypotenuse side and the alpha angle
     public static func opposite(hypotenuse: Double, alpha: Double) -> Double {
-//        alpha = asin(opposite / hypotenuse)
-//        sin(alpha) = opposite / hypotenuse
         return hypotenuse * sin(alpha)
     }
 
@@ -55,28 +55,34 @@ extension Pythagore {
 
     // MARK: - Alpha
 
+    /// Alpha angle from the adjacent and opposite sides
     public static func alpha(adjacent: Double, opposite: Double) -> Double {
         atan(opposite / adjacent)
     }
 
+    /// Alpha angle from the adjacent and hypotenuse sides
     public static func alpha(adjacent: Double, hypotenuse: Double) -> Double {
         acos(adjacent / hypotenuse)
     }
 
+    /// Alpha angle from the opposite and hypotenuse sides
     public static func alpha(opposite: Double, hypotenuse: Double) -> Double {
         asin(opposite / hypotenuse)
     }
 
     // MARK: - Beta
 
+    /// Beta angle from the adjacent and opposite sides
     public static func beta(adjacent: Double, opposite: Double) -> Double {
         atan(adjacent / opposite)
     }
 
+    /// Beta angle from the adjacent and hypotenuse sides
     public static func beta(adjacent: Double, hypotenuse: Double) -> Double {
         asin(adjacent / hypotenuse)
     }
 
+    /// Beta angle from the opposite and hypotenuse sides
     public static func beta(opposite: Double, hypotenuse: Double) -> Double {
         acos(opposite / hypotenuse)
     }
